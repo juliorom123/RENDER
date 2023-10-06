a1e66dfebe8bd5d3cbeabb817a2756f39c2343d2
julio <jromero@itpfp.com>
2023-10-06 14:17:33 +0200

    5commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
+app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  //res.send('Hello World')
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 
-app.listen(3000)
+app.listen(3000);
